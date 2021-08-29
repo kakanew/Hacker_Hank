7f299850f269e3a28d3bc368fdeae8a1fa237ea6
Cassia <camsibr@gmail.com>
2021-08-29 20:45:00 -0300

    Solve Me First

---
+++ Problem_Solving/Solve_Me_First.js
@@ -0,0 +1,12 @@
+
+function solveMeFirst(a, b) {
+    // Hint: Type return a+b below  
+    return a + b;
+}
+
+// Testes IDE Input
+console.log(solveMeFirst(2, 3));
+console.log(solveMeFirst(100, 1000));
+// IDE Output
+// 7
+// 1100
